b50daaa98c44f5836b9012b67ce7105928b96d00
Маккси <Maksim-volkov1994@yandex.ru>
2020-10-17 13:48:16 +0300

    Updates

---
+++ precache-manifest.a1f239be48cf1f793da26cad31e9f4a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707d1451c5e4d4ee663f2eecbaa06e06",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "ed1675fa879eafa23b21",
+    "url": "/ReactApp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "588ae07cf696e92ce348",
+    "url": "/ReactApp/static/css/main.9a268565.chunk.css"
+  },
+  {
+    "revision": "ed1675fa879eafa23b21",
+    "url": "/ReactApp/static/js/2.56866c3c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/ReactApp/static/js/2.56866c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588ae07cf696e92ce348",
+    "url": "/ReactApp/static/js/main.0962f0d9.chunk.js"
+  },
+  {
+    "revision": "64cb09a6e17c3bcf9efb",
+    "url": "/ReactApp/static/js/runtime-main.6b9b2fac.js"
+  }
+]);
