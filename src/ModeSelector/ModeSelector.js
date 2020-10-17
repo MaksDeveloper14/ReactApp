40cadb6a1422fab6c2a1c9585601bde0b7e8a896
MaksDeveloper14 <62217284+MaksDeveloper14@users.noreply.github.com>
2020-10-17 22:18:29 +0300

    Update ModeSelector.js

--- src/ModeSelector/ModeSelector.js
+++ src/ModeSelector/ModeSelector.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default props => {
 
-	const smallUrl = "https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+	const smallUrl = 'https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 
-	const bigUrl = "https://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+	const bigUrl = 'https://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 
 	return (
 		<div style={{display: 'flex', justifyContent: 'center', padding: '50px 0'}}>
